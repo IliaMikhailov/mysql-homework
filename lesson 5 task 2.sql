54300256cc6d1d21d6b247272b65b61a67e2448e
IliaMikhailov <82019331+IliaMikhailov@users.noreply.github.com>
2021-07-19 12:43:11 +0200

    Update lesson 5 task 2.sql

--- lesson 5 task 2.sql
+++ lesson 5 task 2.sql
@@ -1,3 +1,6 @@
+-- Таблица users была неудачно спроектирована. 
+-- Записи created_at и updated_at были заданы типом VARCHAR и в них долгое время помещались значения в формате 20.10.2017 8:10. 
+-- Необходимо преобразовать поля к типу DATETIME, сохранив введённые ранее значения.
 DROP DATABASE IF EXISTS vk3;
 CREATE DATABASE vk3;
 USE vk3;
@@ -42,30 +45,3 @@ ALTER TABLE users MODIFY updated_at DATETIME default now();
 show FIELDS from users;
 
 select * from users;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
