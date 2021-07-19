4eac480b4727a5e0e66f1630c5d828c9d266b311
IliaMikhailov <82019331+IliaMikhailov@users.noreply.github.com>
2021-07-19 12:43:54 +0200

    Update lesson 5 task 3.sql

--- lesson 5 task 3.sql
+++ lesson 5 task 3.sql
@@ -1,3 +1,6 @@
+-- В таблице складских запасов storehouses_products в поле value могут встречаться самые разные цифры: 0, если товар закончился и выше нуля, если на складе имеются запасы. 
+-- Необходимо отсортировать записи таким образом, чтобы они выводились в порядке увеличения значения value. 
+-- Однако нулевые запасы должны выводиться в конце, после всех
 DROP DATABASE IF EXISTS storehouses_products;
 CREATE DATABASE storehouses_products;
 USE storehouses_products;
@@ -15,30 +18,3 @@ VALUES  (0),
 		(1);
 
 select value from products order by if (value > 0, 0, 1), value;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
